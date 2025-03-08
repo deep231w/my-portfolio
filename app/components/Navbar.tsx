8916cfc5000525db23b20df29b4d98cae00ad4e1
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-08 15:31:27 +0530

    updated

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,8 @@
 const Navbar = ()=>{
     return (
         <>
-        <nav>
-            <ul>
+        <nav className="border-b text-red flex justify-end">
+            <ul className="flex space-x-6 pt-2 pb-2 pl-30">
                 <li><a href="/">Home</a></li>
                 <li><a href="/projects">Projects</a></li>
                 <li><a href="/blog">blog</a></li>
