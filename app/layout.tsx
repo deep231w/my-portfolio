e7e579bb220cbed08b7234d892ce1e082cd281bd
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-08 15:02:21 +0530

    navbar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar"
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Navbar/>
       <body>{children}
       </body>
       
