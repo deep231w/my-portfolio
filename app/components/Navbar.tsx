aa475be68d835fc838fd58e47147f78ff57b96f5
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-10 23:50:51 +0530

    updated

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,15 @@
-const Navbar = ()=>{
+const Navbar = () => {
     return (
-        <>
-        <nav className="border-b text-red flex justify-end">
-            <ul className="flex space-x-6 pt-2 pb-2 pl-30">
-                <li><a href="/">Home</a></li>
-                <li><a href="/projects">Projects</a></li>
-                <li><a href="/blog">blog</a></li>
-            </ul>
-        </nav>
-        </>
-    )
-}
+        <div className="w-full bg-black shadow text-white pt-3 pb-3">
+            <nav className="max-w-6xl mx-auto flex items-center">
+                <ul className="flex space-x-6">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/projects">Projects</a></li>
+                    <li><a href="/blog">Blog</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
 
 export default Navbar;
