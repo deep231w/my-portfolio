9ea110a5ef12d10f291219dcd7bc2be855be7b45
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-12 01:39:30 +0530

    ui fixed

--- app/components/ui/GitStats.tsx
+++ app/components/ui/GitStats.tsx
@@ -3,10 +3,10 @@ import GitHubCalendar from "react-github-calendar";
 
 export default function () {
   return (
-    <div className="flex items-center justify-center w-full max-w-6xl mx-auto px-20">
+    <div className="flex items-center justify-center w-full max-w-6xl mx-auto p-5 border-4 border-white rounded-lg text-white">
       <GitHubCalendar
         username="deep231w"
-        blockSize={15}  // Increases the size of contribution squares
+        blockSize={13}  // Increases the size of contribution squares
         blockMargin={5} // Adds spacing between squares
         fontSize={16}   // Adjusts font size for better readability
       />
