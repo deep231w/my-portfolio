44a2bbf21dc9058e6222065330180f238c09375b
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-11 14:13:32 +0530

    ui updated

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,26 @@
+export default function(){
+    return (
+        <section className="flex">
+            <div>
+                <div><h1>Hey there! I’m <b>Deepak</b> 👋</h1></div>
+                <div>
+                    A passionate <b>Softwate Engineer</b>,
+                    open-source contributor, 
+                    and tech enthusiast who loves turning ideas into reality with clean, scalable code.
+                    I thrive on building innovative solutions, contributing to the developer community, and constantly learning new technologies.
+                </div>
+                <ul>
+                    <div>💡 What I bring to the table:</div>
+                    <li>- Proficiency in Next.js, React, TypeScript, Prisma, and Firebase</li>
+                    <li>- Strong backend skills with Node.js and databases (PostgreSQL, MongoDB)</li>
+                    <li>- Open-source contributions and collaboration with developers worldwide</li>
+                    <li>- Interest in DSA (C++), system design, and scalable architectures</li>
+                </ul>
+            </div>
+            
+            <div>
+                <img src="IMG_20231025_215552_679.jpg" alt="profilepic" />
+            </div>
+        </section>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import ContactSection from "./components/contactSection";
+import HeroSection from "./components/HeroSection";
 export default function Home() {
   return (
     <>
       <div>home page</div>
+      <HeroSection/>
       <ContactSection />
     </>
     
