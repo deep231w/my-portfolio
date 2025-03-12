b5a3bfe682697babd6a5e538adbf08824ecbefab
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-12 22:37:23 +0530

    project card updated

--- app/components/ProjectCards.tsx
+++ app/components/ProjectCards.tsx
@@ -1,9 +1,9 @@
 import { ThreeDCardDemo } from "./ThreeDCardDemo";
 export default function (){
     return (
-        <div>
+        <div className="text-white border-white mx-20">
             <div>Projects</div>
-            <ThreeDCardDemo />
+            <ThreeDCardDemo header="Payment App" content="Payment application which has features like p2p transaction" liveLink="Live" githubLink="Github"/>
         </div>
     );
 }
