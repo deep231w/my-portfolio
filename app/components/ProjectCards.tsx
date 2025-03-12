7815e13a802b33610dcfcf56e37a8c8766178a09
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-12 22:27:38 +0530

    ui updated

---
+++ app/components/ProjectCards.tsx
@@ -0,0 +1,9 @@
+import { ThreeDCardDemo } from "./ThreeDCardDemo";
+export default function (){
+    return (
+        <div>
+            <div>Projects</div>
+            <ThreeDCardDemo />
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import ContactSection from "./components/contactSection";
 import HeroSection from "./components/HeroSection";
 import TechStack from "./components/ui/TechStack";
 import GitStats from "./components/ui/GitStats";
+import ProjectCards from "./components/ProjectCards";
 export default function Home() {
   return (
     <div className="relative ">
       <HeroSection />
       <TechStack />
       <GitStats />
+      <ProjectCards/>
     </div>
   );
 }
