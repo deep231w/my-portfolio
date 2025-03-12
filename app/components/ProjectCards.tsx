b56d03a7e89769cb7a6341626930c28012034fd7
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-12 23:02:55 +0530

    card updated

--- app/components/ProjectCards.tsx
+++ app/components/ProjectCards.tsx
@@ -3,7 +3,7 @@ export default function (){
     return (
         <div className="text-white border-white mx-20">
             <div>Projects</div>
-            <div>
+            <div className="flex">
                 <ThreeDCardDemo header="Payment App" content="Payment application which has features like p2p transaction" liveLink="Live" githubLink="Github Repo"/>
                 <ThreeDCardDemo header="DeepBox" content="Code execution platform " liveLink="Live" githubLink="Github Repo"/>
                 <ThreeDCardDemo header="ChatApp" content="Real time chat application using websocket" liveLink="Live" githubLink="GitHub Repo"/>
