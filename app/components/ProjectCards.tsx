1b64e9ee43866aac8c34425af86557591988f2be
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-13 20:47:54 +0530

    ui updated

--- app/components/ProjectCards.tsx
+++ app/components/ProjectCards.tsx
@@ -1,12 +1,12 @@
 import { ThreeDCardDemo } from "./ThreeDCardDemo";
 export default function (){
     return (
-        <div className="text-white border-white mx-20">
+        <div className="text-white border-white w-full max-w-6xl items-center justify-between mx-auto px-20">
             <div>Projects</div>
-            <div className="flex">
-                <ThreeDCardDemo header="Payment App" content="Payment application which has features like p2p transaction" liveLink="Live" githubLink="Github Repo"/>
-                <ThreeDCardDemo header="DeepBox" content="Code execution platform " liveLink="Live" githubLink="Github Repo"/>
-                <ThreeDCardDemo header="ChatApp" content="Real time chat application using websocket" liveLink="Live" githubLink="GitHub Repo"/>
+            <div className="grid grid-cols-2 gap-4 pb-2">
+                <ThreeDCardDemo header="Payment App" content="Payment application which has features like p2p transaction" liveLink="Live" githubLink="Github Repo"image="/Screenshot 2024-10-22 001849.png"/>
+                <ThreeDCardDemo header="DeepBox" content="Code execution platform " liveLink="Live" githubLink="Github Repo" image="/Screenshot 2025-03-13 192922.png"/>
+                <ThreeDCardDemo header="ChatApp" content="Real time chat application using websocket" liveLink="Live" githubLink="GitHub Repo" image="/Screenshot 2025-02-28 102635.png"/> 
             </div>
         </div>
     );
