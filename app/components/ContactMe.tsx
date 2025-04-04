73bd2ba47f73d96c66a90478f6f704dce4862340
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-04-04 23:56:52 +0530

    contat me section added/  typography added

--- app/components/ContactMe.tsx
+++ app/components/ContactMe.tsx
@@ -1,10 +1,34 @@
-
-export default function (){
+import { Input } from "./ui/input";
+export default function ContactMe (){
     return(
         <div className="text-white">
-            <div>Contact Me</div>
             <div>
+                <div>
+                    <div>
+                        <h1>Get in touch</h1>
+                    </div>
+                    <div>
+                            <p>
+                            If you have any inquiries, 
+                            please feel free to reach out.
+                            You can contact me via email at 
+                            maharanad449@gmail.com
+                            </p>
+                    </div>
+                    <div>
+                        Social Handles
+                    </div>
+                </div>
+            
+                <div>
+                    <div>
+                        <Input placeholder="Full Name" />
+                        <Input placeholder="phone No" />
+                    </div>
+                    <div>
 
+                    </div>
+                </div>
             </div>
         </div>
     );
