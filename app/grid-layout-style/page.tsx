347a080361dbff766f30e4ebfcf3aa7d5e010d7f
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-01-29 15:24:33 +0530

    grid layout set

---
+++ app/grid-layout-style/page.tsx
@@ -0,0 +1,11 @@
+export default function GridLayoutStyle(){
+    return(
+        <div className="grid-container">
+            <div className="item" style={{gridArea:"box1"}}>item 1</div>
+            <div className="item" style={{gridArea:"box2"}}>item 2</div>
+            <div className="item" style={{gridArea:"box3"}}>item 3</div>
+            <div className="item" style={{gridArea:"box4"}}>item 4</div>
+            <div className="item" style={{gridArea:"box5"}}>item 5</div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,21 @@ import GitStats from "./components/GitStats";
 import ProjectSection from "./components/ProjectSection";
 import ContactMe from "./components/ContactMe";
 import Experience from "./components/Experience";
+import GridLayoutStyle from "./grid-layout-style/page";
 export default function Home() {
   return (
-    <div className="relative ">
-      <HeroSection />
-      <Experience/>
-      <TechStack />
-      <GitStats />
-      <ProjectSection/>
-      <ContactMe/>
+    //old ui
+    // <div className="relative ">
+    //   <HeroSection />
+    //   <Experience/>
+    //   <TechStack />
+    //   <GitStats />
+    //   <ProjectSection/>
+    //   <ContactMe/>
+    // </div>
+
+    <div className="relative">
+      <GridLayoutStyle/>
     </div>
   );
 } 
