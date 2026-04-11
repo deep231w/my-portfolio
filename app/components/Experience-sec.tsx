21697877aa93f40d72a482cddaf6609fb17ed93d
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-04-11 20:42:55 +0530

    updated

--- app/components/Experience-sec.tsx
+++ app/components/Experience-sec.tsx
@@ -1,7 +1,33 @@
 export default  function ExperienceSec(){
     return(
-        <div>
-             
+        <div
+            className="h-full w-full bg-black flex  flex-col p-5 gap-2"
+        >
+            <h2>Experience</h2>
+            <div>
+                <h2 className="text-xl font-semibold text-gray-400 dark:text-gray-200">
+                    A2D Innovation Pvt. Ltd.
+                </h2>
+                <p className="text-sm text-gray-300 dark:text-gray-400 mb-2">
+                    Frontend Developer (Internship) • April 2025 – September 2025 • Remote
+                </p>
+
+                <ul className="list-disc list-inside space-y-1 text-gray-500 dark:text-gray-300">
+                    <li>
+                        Developed responsive and user-friendly web interfaces using React.js and Material-UI (MUI).
+                    </li>
+                    <li>
+                        Collaborated with design and backend teams to implement seamless UI/UX features.
+                    </li>
+                    <li>
+                        Optimized application performance and ensured cross-browser compatibility.
+                    </li>
+                    <li>
+                        Built a fully responsive, animation-rich website from scratch using React.js, Bootstrap, and GSAP, 
+                        integrating an AI-powered chat feature to enhance customer service.
+                    </li>
+                </ul> 
+            </div> 
         </div>
     )
 }
